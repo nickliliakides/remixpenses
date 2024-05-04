685ea1b3fbf3f9ec581c726bc832264a2d279f8c
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-04 06:56:37 +0100

    Initial commit 10

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
-  serverModuleFormat: "cjs",
+  ignoredRouteFiles: ['**/.*'],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  // serverDependenciesToBundle: []
 };
--- test/setup-test-env.ts
+++
@@ -1,7 +0,0 @@
-const {
-  installGlobals
-} = require("@remix-run/node");
-
-require("@testing-library/jest-dom/vitest");
-
-installGlobals();
