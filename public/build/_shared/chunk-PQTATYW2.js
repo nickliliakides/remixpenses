f79fa481f24b35a5f2de158e1a9d30e7c6625772
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-04 18:11:58 +0100

    add remiw-run/web-fetch

---
+++ public/build/_shared/chunk-PQTATYW2.js
@@ -0,0 +1,15 @@
+import {
+  __commonJS
+} from "/build/_shared/chunk-5KL4PAQL.js";
+
+// empty-module:~/data/expenses.server
+var require_expenses = __commonJS({
+  "empty-module:~/data/expenses.server"(exports, module) {
+    module.exports = {};
+  }
+});
+
+export {
+  require_expenses
+};
+//# sourceMappingURL=/build/_shared/chunk-PQTATYW2.js.map
